6afa1902f1514d389f9c2f4b94c9bf760776342b
Taewoo Lee <oiehot@gmail.com>
2019-12-14 15:04:50 +0900

    exls to json, encoding.json done

--- Asset/ExcelToJson.cs
+++ Asset/ExcelToJson.cs
@@ -1,8 +1,4 @@
-// 에셋 포스트 프로세서
-// .xls 파일이 감지되면 json으로 변환 (외부 쉘 명령으로)
-// ~로 시작하는 .xls 파일은 작업중이므로 무시
-// 날아감 ㅜㅜ
-
+#if false
 using UnityEngine;
 using UnityEditor;
 
@@ -12,28 +8,33 @@ class ExcelToJson : AssetPostprocessor
 	{
 		foreach (string path in importedAssets)
 		{   
-            string ext = System.IO.Path.GetExtension(path);
-            string filename = System.IO.Path.GetFileNameWithoutExtension(path);
-            
-            if ( !ext.ToLower().StartsWith(".xls") ) continue; // 엑셀파일만
-            if ( filename.StartsWith("~") ) continue; // ~로 시작하면 작업중인 파일이므로 무시한다.
-            
-            string projectPath = System.IO.Directory.GetParent(Application.dataPath).FullName;
-            
-            string dir = System.IO.Path.GetDirectoryName(path);
-            const string script = "o:/project/afo2/bin/xls2json.py";
-            string xlsPath = $"{projectPath}/{path}";
-            string jsonPath = $"{projectPath}/{dir}/{filename}.json";
-            
-            // TODO: Custom AssetImport(xls), make property like (bool convertToJson, string worksheetName=StageData)
-            
-            string result;
-            string worksheet = "StageData";
-            int exitCode;
-            
-            exitCode = Rano.OS.Shell.Python(script, $"--xlsPath {xlsPath} --jsonPath {jsonPath} --sheetName {worksheet}", out result);
-            Debug.Log($"Result: {result}");
-            Debug.Log($"Exit code: {exitCode}");
+                  string filename = System.IO.Path.GetFileNameWithoutExtension(path);
+                  if ( filename.StartsWith("~") )
+                  {
+                      Debug.Log($"Ignore: {filename}");
+                      continue; // ~로 시작하면 작업중인 파일이므로 무시한다.
+                  }
+                  
+                  string ext = System.IO.Path.GetExtension(path);           
+                  if ( !ext.ToLower().StartsWith(".xls") ) continue; // 엑셀파일만
+                  
+                  string projectPath = System.IO.Directory.GetParent(Application.dataPath).FullName;
+                  
+                  string dir = System.IO.Path.GetDirectoryName(path);
+                  const string script = "o:/project/afo2/bin/xls2json.py";
+                  string xlsPath = $"{projectPath}/{path}";
+                  string jsonPath = $"{projectPath}/{dir}/{filename}.json";
+                  
+                  // TODO: Custom AssetImport(xls), make property like (bool convertToJson, string worksheetName=StageData)
+                  
+                  string result;
+                  string worksheet = "StageData";
+                  int exitCode;
+                  
+                  exitCode = Rano.OS.Shell.Python(script, $"--xlsPath {xlsPath} --jsonPath {jsonPath} --sheetName {worksheet}", out result);
+                  Debug.Log($"Result: {result}");
+                  Debug.Log($"Exit code: {exitCode}");
 		}
 	}
 }
+#endif
---
+++ Encoding/Json.cs
@@ -0,0 +1,39 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rano.Encoding
+{
+    [System.Serializable]
+    public class ArrayWrapper<T>
+    {
+        public T[] items;
+    }
+
+    public static class Json
+    {
+        
+        public static T FromJson<T>(string jsonText)
+        {
+            T obj = JsonUtility.FromJson<T>(jsonText);
+            return obj;
+        }
+        public static T[] FromArrayJson<T>(string jsonText)
+        {
+            ArrayWrapper<T> a = JsonUtility.FromJson< ArrayWrapper<T> >(jsonText);
+            return a.items;
+        }
+        
+        public static string ToJson<T>(T obj, bool prettyPrint=true)
+        {
+            return JsonUtility.ToJson(obj, prettyPrint);
+        }
+        
+        public static string ToJson<T>(T[] array, bool prettyPrint=true)
+        {
+            ArrayWrapper<T> wrapper = new ArrayWrapper<T>();
+            wrapper.items = array;
+            return JsonUtility.ToJson(wrapper, prettyPrint);
+        }
+    }
+}
