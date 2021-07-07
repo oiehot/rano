3adbaa3e6ff35b41042a88ea014a28b1e84c161e
TaewooLee <oiehot@gmail.com>
2021-07-07 13:18:26 +0900

    LocalSave: Json Pretty 매개변수 옵션 추가. Save/Load Binary 기능 추가.

--- Runtime/File/LocalSave.cs
+++ Runtime/File/LocalSave.cs
@@ -4,11 +4,13 @@
 // Written by Taewoo Lee <oiehot@gmail.com>
 
 using System;
+using System.IO;
 using System.Text; // Encoding
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
+// using UnityEditor;
 // using Newtonsoft.Json;
 
 namespace Rano.File
@@ -22,12 +24,22 @@ namespace Rano.File
             System.IO.File.WriteAllBytes(path, bytes);
         }
         
-        public static void SaveToJson<T>(string filePath, T data)
+        public static void SaveToJson<T>(string filePath, T data, bool prettyPrint=true)
         {
             // Newtonsoft.Json 을 사용하는 경우: string json = JsonConvert.SerializeObject(data);
-            string txt = JsonUtility.ToJson(data, false); // prettyPrint: True
+            string txt = JsonUtility.ToJson(data, prettyPrint);
             byte[] bytes = System.Text.Encoding.UTF8.GetBytes(txt);
             Save(filePath, bytes);
+        }
+
+        public static void SaveToBinary<T>(string filePath, T data)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            string path = $"{Application.persistentDataPath}/{filePath}";
+            FileStream stream = new FileStream(path, FileMode.Create);
+            formatter.Serialize(stream, data);
+            stream.Close();
         }
         
         public static byte[] Load(string filePath)
@@ -35,7 +47,7 @@ namespace Rano.File
             string path = $"{Application.persistentDataPath}/{filePath}";
             if (!System.IO.File.Exists(path))
             {
-                throw new Exception("File is not exists");
+                throw new System.IO.FileNotFoundException($"로드할 파일을 찾지 못했습니다: {filePath}");
             }
             
             Log.Info($"Load from {path}");
@@ -48,7 +60,7 @@ namespace Rano.File
             string path = $"{Application.persistentDataPath}/{filePath}";
             if (!System.IO.File.Exists(path))
             {
-                throw new Exception("File is not exists");
+                throw new System.IO.FileNotFoundException($"로드할 Json 파일을 찾지 못했습니다: {filePath}");
             }
             
             byte[] bytes;
@@ -56,6 +68,23 @@ namespace Rano.File
             string json = System.Text.Encoding.UTF8.GetString(bytes);
 
             return JsonUtility.FromJson<T>(json);
-        }     
+        }
+
+        public static T LoadFromBinary<T>(string filePath)
+        {
+            string path = $"{Application.persistentDataPath}/{filePath}";
+            if (System.IO.File.Exists(path))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                FileStream stream = new FileStream(path, FileMode.Open);
+                T data = (T)formatter.Deserialize(stream);
+                return data;
+            }
+            else
+            {
+                throw new System.IO.FileNotFoundException($"로드할 Json 파일을 찾지 못했습니다: {filePath}");
+                // return null;
+            }
+        }
     }
 }
