44ec96e5191e653d918ca4dae39ef950e29d9716
TaewooLee <oiehot@gmail.com>
2021-11-16 16:57:06 +0900

    GameObject, Transform 에서 계층경로를 리턴하는 확장 메서드 추가.

--- Runtime/Extensions/GameObjectExtensions.cs
+++ Runtime/Extensions/GameObjectExtensions.cs
@@ -10,12 +10,28 @@ namespace Rano
     public static class GameObjectExtensions
     {
         /// <summary>
+        /// Hierarchy상 게임오브젝트의 경로를 돌려준다.
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// </example>
+        public static string GetPath(this GameObject gameObject)
+        {
+            string path = "/" + gameObject.name;
+            while (gameObject.transform.parent != null)
+            {
+                gameObject = gameObject.transform.parent.gameObject;
+                path = "/" + gameObject.name + path;
+            }
+            return path;
+        }
+
+        /// <summary>
         /// 게임오브젝트에 부착된 컴포넌트를 돌려준다. 없으면 새로 생성해서 돌려준다.
         /// </summary>
         /// <typeparam name="T">돌려줄 컴포넌트 타입.</typeparam>
         /// <param name="gameObject">해당 컴포넌트가 부착된 게임오브젝트.</param>
         /// <returns>컴포넌트</returns>
-        static public T GetOrAddComponent<T>(this GameObject gameObject) where T : Component
+        public static T GetOrAddComponent<T>(this GameObject gameObject) where T : Component
         {
             if (gameObject.TryGetComponent<T>(out var component))
                 return component;
--- Runtime/Extensions/TransformExtensions.cs
+++ Runtime/Extensions/TransformExtensions.cs
@@ -9,6 +9,26 @@ namespace Rano
 {    
     public static class TransformExtensions
     {
+        /// <summary>
+        /// Hierarchy상 게임오브젝트의 경로를 돌려준다.
+        /// </summary>
+        /// <param name="gameObject"></param>
+        /// </example>
+        public static string GetPath(this Transform transform)
+        {
+            string path = transform.name;
+            while (transform.parent != null)
+            {
+                transform = transform.parent;
+                path = transform.name + "/" + path;
+            }
+            return path;
+        }
+
+        /// <summary>
+        /// Transform 값을 초기화한다.
+        /// </summary>
+        /// <param name="trans"></param>
         public static void ResetTransformation(this Transform trans)
         {
             trans.position = Vector3.zero;
