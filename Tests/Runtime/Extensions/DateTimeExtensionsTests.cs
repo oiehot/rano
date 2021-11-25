aef091fa940fb63e3b091d6724deae53cc72ec94
TaewooLee <oiehot@gmail.com>
2021-11-25 17:26:50 +0900

    TimeSpan: 사람이 읽기쉬운 String을 리턴하는 확장 메서드 추가.

---
+++ Tests/Runtime/Extensions/DateTimeExtensionsTests.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (C) OIEHOT - All Rights Reserved
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+// Written by Taewoo Lee <oiehot@gmail.com>
+
+using Rano;
+using System;
+using NUnit.Framework;
+
+namespace Rano.Tests.Extensions
+{
+    public class DateTimeExtensionsTest
+    {
+        [Test]
+        public void TimeSpan_ToHumanReadbleString_Test()
+        {
+            TimeSpan timeSpan;
+
+            timeSpan = new TimeSpan(hours: 0, minutes: 0, seconds: 0);
+            Assert.AreEqual(
+                timeSpan.ToHumanReadableString(),
+                ""
+            );
+
+            timeSpan = new TimeSpan(hours: 23, minutes: 59, seconds: 59);
+            Assert.AreEqual(
+                timeSpan.ToHumanReadableString(),
+                "23 hours 59 minutes 59 seconds"
+            );
+
+            timeSpan = new TimeSpan(hours: 1, minutes: 1, seconds: 1);
+            Assert.AreEqual(
+                timeSpan.ToHumanReadableString(),
+                "1 hour 1 minute 1 second"
+            );
+
+
+            timeSpan = new TimeSpan(hours: 24, minutes: 0, seconds: 0);
+            Assert.AreEqual(
+                timeSpan.ToHumanReadableString(),
+                "1 day"
+            );
+
+            timeSpan = new TimeSpan(hours: 48, minutes: 0, seconds: 0);
+            Assert.AreEqual(
+                timeSpan.ToHumanReadableString(),
+                "2 days"
+            );
+        }
+
+        [Test]
+        public void TimeSpan_ToReadbleString_Test()
+        {
+            TimeSpan timeSpan;
+
+            timeSpan = new TimeSpan(hours: 0, minutes: 0, seconds: 0);
+            Assert.AreEqual(
+                timeSpan.ToReadableString(),
+                "00:00:00"
+            );
+
+            timeSpan = new TimeSpan(hours: 23, minutes: 59, seconds: 59);
+            Assert.AreEqual(
+                timeSpan.ToReadableString(),
+                "23:59:59"
+            );
+
+            timeSpan = new TimeSpan(hours: 24, minutes: 00, seconds: 00);
+            Assert.AreEqual(
+                timeSpan.ToReadableString(),
+                "00:00:00"
+            );
+        }
+    }
+}
