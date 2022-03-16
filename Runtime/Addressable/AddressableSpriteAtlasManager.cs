f6bed5d872835db535ffc034beb9768672e3a73e
Taewoo Lee <oiehot@gmail.com>
2022-03-16 20:28:50 +0900

    어드레서블 SpriteAtlas 로드시 SpritAtlasManager.atlasRequest 가 호출되어 등록되는 것을 발견하여 콜백을 보관하여 일괄적으로 로드했던 기존 코드를 제거함.

--- Runtime/Addressable/AddressableSpriteAtlasManager.cs
+++ Runtime/Addressable/AddressableSpriteAtlasManager.cs
@@ -10,22 +10,37 @@ using Rano;
 
 namespace Rano.Addressable
 {
-    /// <summary>
-    /// 스프라이트 아틀라스를 빌트인되지 않기 위해서 Include in Build 를 활성화 하는 경우,
-    /// 특별한 처리없이 아틀라스내에 있는 스프라이트 사용시 에러가 난다.
-    /// 아틀라스에 들어가 있는 스프라이트를 로드하기 위해서 아틀라스에 처음 접근할 때 SpriteAtlasManager.atlasRequested가 호출되는데
-    /// 여기서 어드레서블을 통해 로드한 아틀라스를 전달해주면 정상적으로 스프라이트가 로드된다.
-    /// </summary>
+    /// <remarks>
+    /// SpriteAtlasManager.atlasRequested 이슈:
+    ///
+    /// SpriteAtlas의 에셋관리를 위해서 Include In Build를 비활성화 하는 경우
+    /// SpriteAtlas에 연결된 Sprite가 자동으로 로드되지 않아 백색사각형으로 나오는 경우가 있다.
+    ///
+    /// 이러한 문제를 해결하기 위해서 SpriteAtlasManager는 씬이 시작될 때
+    /// 모든 Sprite(SpriteAtlas에 속한)를 SpriteAtlas의 Sprite로 교체하는데
+    /// SpriteAtlas를 얻기 위해서 사용자에게 요청한다.
+    ///
+    /// 사용자는 이 요청에 콜백을 달아 응답할 수 있는데,
+    /// SpriteAtlasManager.atlasRequested 에 이벤트 핸들러를 추가함으로 가능하다.
+    /// 이벤트 핸들러에서 Addressables.AssetLoadAsync 등으로 SpriteAtlas 에셋을 로드하고,
+    /// 넘어온 콜백에 SpriteAtlas를 매개변수로 담아 호출해주면 SpriteAtlasManager에 SpriteAtlas가 등록되고
+    /// 모든 Sprite(SpriteAtlas에 속한)가 SpriteAtlas에 들어있는 Sprite로 교체된다.
+    ///
+    /// 또한,
+    /// Addressables.AssetLoadAsync를 사용해서 SpriteAtlas를 로드하면
+    /// 씬이 시작됨과 관계없이 다시한번 SpriteAtlasManager.atlasRequested가 호출된다.
+    /// 여기서 로드시 미리 캐싱해둔 SpriteAtlas를 콜백에 전달하면 Sprite들이 정상적으로 출력된다.
+    ///
+    /// 그러므로 SpriteAtlasManager.atlasRequsted가 콜백되는 시점은 다음 두 가지다:
+    /// 1) 씬이 시작될 때
+    /// 2) Addressables.AssetLoadAsync로 SpriteAtlas 에셋이 로드될 때
+    ///
+    /// </remarks>
+    // TODO: AddressableAssetManager에 포함시킬것.
     public class AddressableSpriteAtlasManager : MonoSingleton<AddressableSpriteAtlasManager>
     {
-        private Dictionary<string, System.Action<SpriteAtlas>> _atlasRequests = new Dictionary<string, System.Action<SpriteAtlas>>();
+        // private Dictionary<string, System.Action<SpriteAtlas>> _atlasRequests = new Dictionary<string, System.Action<SpriteAtlas>>();
         private Dictionary<string, SpriteAtlas> _spriteAtlases = new Dictionary<string, SpriteAtlas>();
-
-        /// <summary>
-        /// SpriteAtlasManager의 요청이 들어오면 즉시 로드할지 여부. 활성화시 즉시 로드되고
-        /// 비활성화하면 수동으로 로드해야 한다. (LoadAllRequstedSpriteAtlas를 사용하면 된다)
-        /// </summary>
-        public bool IsAutoLoad { get; private set; } = false;
 
         protected override void OnEnable()
         {
@@ -47,50 +62,28 @@ namespace Rano.Addressable
 
             if (TryGetLoadedSpriteAtlas(name, out spriteAtlas))
             {
-                Log.Important($"SpriteAtlas Requested: {name} => AlreadyLoaded => SendToCallback");
+                Log.Info($"SpriteAtlas is requested, Sends the Loaded SpriteAtlas ({name})");
                 callback(spriteAtlas);
                 return;
             }
             else
             {
-                if (IsAutoLoad)
-                {
-                    Log.Important($"SpriteAtlas Requested: {name} => AutoLoad => LoadImmediately");
-                    LoadRequestedSpriteAtlasAsync(name, callback).ConfigureAwait(false);
-                }
-                else
-                {
-                    Log.Important($"SpriteAtlas Requested: {name}");
-                    _atlasRequests[name] = callback;
-                }
+                // 이 메시지가 출력되지 않게 하려면 SpriteAtlas를 미리 로드하면 된다.
+                Log.Warning($"SpriteAtlas is requested, But SpriteAtlas is not Loaded ({name})");
+                // _atlasRequests[name] = callback;
             }
         }
 
         private void HandleAtlasRegistered(SpriteAtlas spriteAtlas)
         {
-            Log.Info($"SpriteAtlas Registered: {spriteAtlas}");
+            Log.Info($"SpriteAtlas Registered ({spriteAtlas})");
         }
 
-        /// <summary>
-        /// SpriteAtlasManager에 의해 요청된 SpriteAtlas를 즉시 로딩시작하고 완료되는 즉시 콜백으로 전달하게 한다.
-        /// </sumamry>
-        private async Task LoadRequestedSpriteAtlasAsync(string name, System.Action<SpriteAtlas> callback)
-        {
-            var asyncOperation = Addressables.LoadAssetAsync<SpriteAtlas>(name);
-            await asyncOperation.Task;
-            if (asyncOperation.Status == AsyncOperationStatus.Succeeded)
-            {
-                Log.Info($"SpriteAtlas Loaded: {name}");
-                var spriteAtlas = asyncOperation.Result;
-                _spriteAtlases[name] = spriteAtlas;
-                callback(spriteAtlas);
-            }
-        }
-
+#if false
         /// <summary>
         /// SpriteAtlasManager에 의해 요청된 모든 SpriteAtlas들을 로딩하고 콜백으로 전달한다.
         /// </sumamry>
-        public async Task LoadAllRequstedSpriteAtlasAsync()
+        public async Task LoadAllRequestedSpriteAtlasAsync()
         {
             var tasks = new List<Task>();
 
@@ -101,11 +94,15 @@ namespace Rano.Addressable
                 var name = request.Key;
                 var callback = request.Value;
 
+                if (IsSpriteAtlasLoaded(name))
+                {
+                    throw new Exception();
+                }
                 var loadSpriteAtlas = Addressables.LoadAssetAsync<SpriteAtlas>(name);
                 loadSpriteAtlas.Completed += (handle) => {
                     if (handle.Status == AsyncOperationStatus.Succeeded)
                     {
-                        Log.Info($"SpriteAtlas Loaded: {name}");
+                        Log.Info($"SpriteAtlas Loaded ({name})");
                         SpriteAtlas spriteAtlas = handle.Result;
                         callback(spriteAtlas);
                         _spriteAtlases[name] = spriteAtlas;
@@ -122,12 +119,17 @@ namespace Rano.Addressable
 
             _atlasRequests.Clear();
         }
+#endif
 
+        /// <summary>
+        /// 어드레서블 SpriteAtlas 에셋을 로드하여 캐싱한다.
+        /// 로드되면 자동으로 SpriteAtlasManager.atlasRequested가 호출되어 전달된다.
+        /// </summary>
         public async Task LoadSpriteAtlasAsync(string name)
         {
             if (IsSpriteAtlasLoaded(name))
             {
-                Log.Warning($"SpriteAtlas Already Loaded: {name}");
+                Log.Warning($"SpriteAtlas is already Loaded ({name})");
                 return;
             }
 
@@ -135,7 +137,7 @@ namespace Rano.Addressable
             await handle.Task;
             if (handle.Status == AsyncOperationStatus.Succeeded)
             {
-                Log.Info($"SpriteAtlas Loaded: {name}");
+                Log.Info($"SpriteAtlas Loaded ({name})");
                 _spriteAtlases[name] = handle.Result;
             }
         }
@@ -145,7 +147,7 @@ namespace Rano.Addressable
             SpriteAtlas spriteAtlas;
             if (TryGetLoadedSpriteAtlas(name, out spriteAtlas))
             {
-                Log.Info($"Unload SpriteAtlas: {name}");
+                Log.Info($"Unload SpriteAtlas ({name})");
                 Addressables.Release(spriteAtlas);
                 _spriteAtlases.Remove(name);
             }
@@ -156,11 +158,11 @@ namespace Rano.Addressable
             Log.Info("Unload All SpriteAtlas");
             foreach (var kv in _spriteAtlases)
             {
-                Log.Info($"Unload SpriteAtlas: {kv.Key}");
+                Log.Info($"Unload SpriteAtlas ({kv.Key})");
                 Addressables.Release(kv.Value);
             }
-            _spriteAtlases.Clear();        
-            _atlasRequests.Clear();
+            _spriteAtlases.Clear();
+            // _atlasRequests.Clear();
         }
 
         public bool TryGetLoadedSpriteAtlas(string name, out SpriteAtlas value)
