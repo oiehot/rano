bca522b3e9b90a4e48904aef19d43fa7a53382d4
Taewoo Lee <oiehot@gmail.com>
2022-04-01 19:40:25 +0900

    싱글톤 ScriptableObject 템플릿 클래스 추가

---
+++ Runtime/Pattern/SingletonScriptableObject.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (C) OIEHOT - All Rights Reserved
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+// Written by Taewoo Lee <oiehot@gmail.com>
+
+using System;
+using UnityEngine;
+
+namespace Rano
+{
+    public abstract class SingletonScriptableObject<T> : ScriptableObject where T : ScriptableObject
+    {
+        private static T _instance;
+        private static readonly object Lock = new object();
+
+        public static T Instance
+        {
+            get
+            {
+                lock (Lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = Resources.Load(typeof(T).Name) as T;
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Instance를 생성하기 위한 빈 메소드.
+        /// </summary>
+        public void EmptyMethod()
+        {
+        }
+    }
+}
