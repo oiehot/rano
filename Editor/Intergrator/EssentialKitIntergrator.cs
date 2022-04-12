e9c9d12a39a42a9b6efff7dfe04ea29f033570f2
Taewoo Lee <oiehot@gmail.com>
2022-04-12 10:53:23 +0900

    자체 설정값(스크립터블오브젝트)를 플레이 시 에드몹으로 자동으로 통합시키는 클래스 추가

--- Editor/Intergrator/EssentialKitIntergrator.cs
+++ Editor/Intergrator/EssentialKitIntergrator.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 using UnityEditor;
 using Rano.PlatformServices.Billing;
 using Rano.PlatformServices.Gaming;
+using RanoEditor.Helper;
 using VoxelBusters.CoreLibrary.NativePlugins;
 using VoxelBusters.EssentialKit;
 
 namespace RanoEditor.Intergrator
 {
     /// <summary>
-    /// InAppProduct, Achievement, Leaderboard 스크립터블 오브젝트 데이터를 모아서 EssentialKitSettings를 업데이트한다.
+    /// InAppProductSO, AchievementSO, LeaderboardSO 데이터를 모아서 EssentialKitSettings를 업데이트한다.
     /// </summary>
     /// <remarks>에디터에서 플레이 전후로 실행된다.</remarks>
     public static class EssentialKitIntergrator
@@ -18,10 +19,14 @@ namespace RanoEditor.Intergrator
         [InitializeOnLoadMethod]
         private static void Intergrate()
         {
-            Debug.Log("Intergrating VoxelBustersEssentialKit...");
-            var settings = GetEssentialKitSettings();
-            IntergrateInAppProducts(settings);
-            IntergrateAchievementsAndLeaderboards(settings);
+            var essentialKitSettings = AssetDatabaseHelper.GetScriptableObject<EssentialKitSettings>();
+            if (essentialKitSettings == null)
+            {
+                Debug.Log($"{typeof(EssentialKitSettings).FullName} 스크립터블 오브젝트가 없으므로 설정통합을 생략합니다.");
+                return;
+            }
+            IntergrateInAppProducts(essentialKitSettings);
+            IntergrateAchievementsAndLeaderboards(essentialKitSettings);
         }
         
         /// <summary>
@@ -29,12 +34,11 @@ namespace RanoEditor.Intergrator
         /// </summary>
         private static void IntergrateInAppProducts(EssentialKitSettings settings)
         {
-            Debug.Log("Intergrating InAppProducts to VoxelBustersEssentialKit...");
-            
+            Debug.Log($"Intergrating {nameof(InAppProductSO)} to {nameof(VoxelBusters.EssentialKit)}...");
             var before = settings.BillingServicesSettings;
             var billingServiceSettings = new BillingServicesUnitySettings (
                 before.IsEnabled,
-                GetBillingProductDefinitons().ToArray(),
+                Get_Intergrated_BillingProductDefinitons().ToArray(),
                 before.MaintainPurchaseHistory,
                 before.AutoFinishTransactions,
                 before.VerifyPaymentReceipts,
@@ -45,52 +49,62 @@ namespace RanoEditor.Intergrator
         }
 
         /// <summary>
+        /// GpgsSettingsSO 데이터를 통합한 EssentialKitSettings.GameServicesSettings.AndroidProperties을 리턴한다.
+        /// </summary>
+        private static VoxelBusters.EssentialKit.GameServicesUnitySettings.AndroidPlatformProperties
+            Get_GameServices_AndroidPlatformProperties_Intergrated(EssentialKitSettings settings)
+        {
+            var gpgsSettings = AssetDatabaseHelper.GetScriptableObject<GpgsSettingsSO>();
+            if (gpgsSettings == null) return null;
+            var before = settings.GameServicesSettings.AndroidProperties;
+            var androidPlatformProperties =
+                new VoxelBusters.EssentialKit.GameServicesUnitySettings.AndroidPlatformProperties(
+                    gpgsSettings.gpgsApplicationId,
+                    before.ServerClientId,
+                    before.AchievedDescriptionFormats,
+                    before.ShowErrorDialogs,
+                    before.DisplayPopupsAtTop,
+                    before.NeedsProfileScope,
+                    before.NeedsEmailScope
+                );
+            return androidPlatformProperties;
+        }
+
+        /// <summary>
         /// AchievementSO, LeaderboardSO 데이터를 EssentialKitSettings.GameServicesSettings에 통합시킨다.
         /// </summary>
         private static void IntergrateAchievementsAndLeaderboards(EssentialKitSettings settings)
         {
-            // TODO: 에드몹 ID 통합 필요
-            Debug.LogWarning("Intergration Required: <b>AndroidProperties</b> (GoogleAdmob => EssentialKitSettings.GameServicesSettings, if null then err)");
-            Debug.Log("Intergrating Achievements and Leaderboards to VoxelBustersEssentialKit...");
+            Debug.Log($"Intergrating {nameof(AchievementSO)} and {nameof(LeaderboardSO)} to {nameof(VoxelBusters.EssentialKit)}...");
             var before = settings.GameServicesSettings;
+            
+            var androidPlatformProperties = Get_GameServices_AndroidPlatformProperties_Intergrated(settings);
+            if (androidPlatformProperties == null)
+            {
+                Debug.LogWarning($"{typeof(GpgsSettingsSO).FullName}가 없습니다. 생성하고 설정하시기를 추천합니다. 설정통합을 생략합니다");
+                androidPlatformProperties = before.AndroidProperties;
+            }
+            
             var gameServicesSettings = new GameServicesUnitySettings(
                 before.IsEnabled,
                 true,
-                GetLeaderboardDefinitons().ToArray(),
-                GetAchievementDefinitons().ToArray(),
+                Get_LeaderboardDefinitons_Intergrated().ToArray(),
+                Get_AchievementDefinitons_Intergrated().ToArray(),
                 before.ShowAchievementCompletionBanner,
-                before.AndroidProperties
+                androidPlatformProperties
             );
             settings.GameServicesSettings = gameServicesSettings;
-        }        
-        
-        /// <summary>
-        /// EssentialKitSettings 에셋을 로드해서 리턴한다.
-        /// </summary>
-        private static EssentialKitSettings GetEssentialKitSettings()
-        {
-            var scriptableObjectName = nameof(EssentialKitSettings);
-            var guids = AssetDatabase.FindAssets($"t:{scriptableObjectName}");
-            if (guids.Length > 1) throw new Exception($"{scriptableObjectName} 에셋이 1개를 초과할 수는 없습니다");
-            var guid = guids[0];
-            var path = AssetDatabase.GUIDToAssetPath(guid);
-            var settings = AssetDatabase.LoadAssetAtPath(path, typeof(EssentialKitSettings)) as EssentialKitSettings;
-            return settings;
         }
 
         /// <summary>
-        /// LeaderboardSO 에셋들을 모아서 LeaderboardDefinition 리스트로 변환한다.
+        /// LeaderboardSO 데이터를 통합한 LeaderboardDefinition 리스트를 리턴한다.
         /// </summary>
-        private static List<LeaderboardDefinition> GetLeaderboardDefinitons()
+        private static List<LeaderboardDefinition> Get_LeaderboardDefinitons_Intergrated()
         {
-            var scriptableObjectName = nameof(LeaderboardSO);
             var leaderboardList = new List<LeaderboardDefinition>();
-            var guids = AssetDatabase.FindAssets($"t:{scriptableObjectName}");
-            foreach (var guid in guids)
+            var leaderboards = AssetDatabaseHelper.GetScriptableObjects<LeaderboardSO>();
+            foreach (var leaderboard in leaderboards)
             {
-                var path = AssetDatabase.GUIDToAssetPath(guid);
-                var leaderboard = AssetDatabase.LoadAssetAtPath(path, typeof(LeaderboardSO)) as LeaderboardSO;
-                if (leaderboard == null) throw new Exception($"Missing {scriptableObjectName} ({path}, {guid}");
                 var platformIdOverrides = new NativePlatformConstantSet(
                     leaderboard.iosId,
                     leaderboard.tvosId,
@@ -104,25 +118,21 @@ namespace RanoEditor.Intergrator
                         null,
                         null
                 );
-                Debug.Log($"* {leaderboard.id} (Leaderboard)");
+                // Debug.Log($"* {leaderboard.id} (Leaderboard)");
                 leaderboardList.Add(product);
             }
             return leaderboardList;
         }
         
         /// <summary>
-        /// AchievementSO 에셋들을 모아서 AchievementDefinition 리스트로 변환한다.
+        /// AchievementSO 데이터를 통합한 AchievementDefinition 리스트를 리턴한다.
         /// </summary>
-        private static List<AchievementDefinition> GetAchievementDefinitons()
+        private static List<AchievementDefinition> Get_AchievementDefinitons_Intergrated()
         {
-            var scriptableObjectName = nameof(AchievementSO);
             var achievementList = new List<AchievementDefinition>();
-            var guids = AssetDatabase.FindAssets($"t:{scriptableObjectName}");
-            foreach (var guid in guids)
+            var achievements = AssetDatabaseHelper.GetScriptableObjects<AchievementSO>();
+            foreach (var achievement in achievements)
             {
-                var path = AssetDatabase.GUIDToAssetPath(guid);
-                var achievement = AssetDatabase.LoadAssetAtPath(path, typeof(AchievementSO)) as AchievementSO;
-                if (achievement == null) throw new Exception($"Missing {scriptableObjectName} ({path}, {guid}");
                 var platformIdOverrides = new NativePlatformConstantSet(
                     achievement.iosId,
                     achievement.tvosId,
@@ -137,34 +147,26 @@ namespace RanoEditor.Intergrator
                     null,
                     null
                 );
-                Debug.Log($"* {achievement.id} (Achievement)");
+                // Debug.Log($"* {achievement.id} (Achievement)");
                 achievementList.Add(product);
             }
             return achievementList;
         }
 
         /// <summary>
-        /// InAppProductSO 에셋들을 모아서 BillingProductDefinition 리스트로 변환한다.
+        /// InAppProductSO 데이터를 통합한 BillingProductDefinition 리스트를 리턴한다. 
         /// </summary>
-        private static List<BillingProductDefinition> GetBillingProductDefinitons()
+        private static List<BillingProductDefinition> Get_Intergrated_BillingProductDefinitons()
         {
-            var scriptableObjectName = nameof(InAppProductSO);
             var productList = new List<BillingProductDefinition>();
-            var guids = AssetDatabase.FindAssets($"t:{scriptableObjectName}");
-            foreach (var guid in guids)
+            var iaps = AssetDatabaseHelper.GetScriptableObjects<InAppProductSO>();
+            foreach (var iap in iaps)
             {
-                var path = AssetDatabase.GUIDToAssetPath(guid);
-                var iap = AssetDatabase.LoadAssetAtPath(path, typeof(InAppProductSO)) as InAppProductSO;
-                if (iap == null)
-                {
-                    throw new Exception($"Missing {scriptableObjectName} ({path}, {guid}");
-                }
                 var platformIdOverrides = new NativePlatformConstantSet(
                     null,
                     null,
                     null
                 );
-                
                 BillingProductType billingProductType;
                 switch (iap.productType)
                 {
@@ -188,7 +190,7 @@ namespace RanoEditor.Intergrator
                     null,
                     null
                 );
-                Debug.Log($"* {iap.id} (InAppProduct)");
+                // Debug.Log($"* {iap.id} (InAppProduct)");
                 productList.Add(product);
             }
             return productList;
