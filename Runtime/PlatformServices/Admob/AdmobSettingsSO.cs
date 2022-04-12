246dca4a7c44ea5bfe83605ca659433ee3930c11
Taewoo Lee <oiehot@gmail.com>
2022-04-12 10:51:51 +0900

    설정용 스크립터블 오브젝트 추가

---
+++ Runtime/PlatformServices/Admob/AdmobSettingsSO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using GoogleMobileAds.Api;
+using UnityEngine;
+
+namespace Rano.PlatformServices.Admob
+{
+    [CreateAssetMenu(fileName = "AdmobSettings", menuName = "Rano/Settings/Admob Settings")]
+    public class AdmobSettingsSO : ScriptableObject
+    {
+        public string iosId;
+        public string androidId; // ex) ca-app-pub-****14***60****5~9048***80*
+        public bool delayAppMeasurement;
+    }
+}
---
+++ Runtime/PlatformServices/Gaming/GpgsSettingsSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Rano.PlatformServices.Gaming
+{
+    [CreateAssetMenu(fileName = "GpgsSettings", menuName = "Rano/Settings/GPGS Settings")]
+    public class GpgsSettingsSO : ScriptableObject
+    {
+        public string gpgsApplicationId; // ex) *3***1687**
+    }
+}
