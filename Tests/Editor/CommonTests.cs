2bb52fd29d95332421303a8fe5598113c680588f
Taewoo Lee <oiehot@gmail.com>
2022-04-13 17:39:06 +0900

    에디터용 빈 테스트 추가

---
+++ Tests/Editor/CommonTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Rano.Editor.Tests
+{
+    public sealed class CommonTests
+    {
+        [Test]
+        public void DefaultTest()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
