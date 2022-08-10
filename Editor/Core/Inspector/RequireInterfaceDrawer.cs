372db41bd70bc6a2a349d01a917c1b55aa1ba1da
Taewoo Lee <oiehot@gmail.com>
2022-08-10 11:39:42 +0900

    인터페이스 객체를 인스펙터에 지정할 수 있게 만드는 Attribute와 Drawer 테스트 (사용하지는 않을 계획)

---
+++ Editor/Core/Inspector/RequireInterfaceDrawer.cs
@@ -0,0 +1,40 @@
+﻿#if false
+
+// Ref: https://www.patrykgalach.com/2020/01/27/assigning-interface-in-unity-inspector/
+
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Scripting;
+
+namespace Rano.Editor.Inspector
+{
+    [CustomPropertyDrawer(typeof(RequiredInterfaceAttribute))]
+    public class RequiredInterfaceAttribute : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (property.propertyType == SerializedPropertyType.ObjectReference)
+            {
+                var requiredAttribute = this.attribute as RequireInterfaceAttribute;
+                EditorGUI.BeginProperty(position, label, property);
+                property.objectReferenceValue = EditorGUI.ObjectField(
+                    position,
+                    label,
+                    property.objectReferenceValue,
+                    requiredAttribute.requiredType,
+                    true
+                );
+                EditorGUI.EndProperty();
+            }
+            else
+            {
+                var previousColor = GUI.color;
+                GUI.color = Color.red;
+                EditorGUI.LabelField(position, label, new GUIContent("Property is not a reference type"));
+                GUI.color = previousColor;
+            }
+        }        
+    }
+}
+
+#endif
---
+++ Runtime/Core/Attributes/RequireInterfaceAttribute.cs
@@ -0,0 +1,20 @@
+﻿#if false
+
+// Ref: https://www.patrykgalach.com/2020/01/27/assigning-interface-in-unity-inspector/
+
+using UnityEngine;
+
+namespace Rano
+{
+    public class RequireInterfaceAttribute : PropertyAttribute
+    {
+        public System.Type requiredType { get; private set; }
+
+        public RequireInterfaceAttribute(System.Type type)
+        {
+            this.requiredType = type;
+        }
+    }
+}
+
+#endif
