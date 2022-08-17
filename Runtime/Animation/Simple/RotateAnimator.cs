2f9db7c63b1fc966d130a397eb1d4a8a70f3a20c
Taewoo Lee <oiehot@gmail.com>
2022-08-17 14:46:19 +0900

    RoatetAnimator 단순한 회전 컴포넌트 추가.

---
+++ Runtime/Animation/Simple/RotateAnimator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Rano.Animation
+{
+    public class RotateAnimator : MonoBehaviour
+    {
+        [SerializeField] private Vector3 _rotatePerSecond;
+
+        void Update()
+        {
+            Vector3 moveRotate = _rotatePerSecond * Time.deltaTime;
+            transform.Rotate(moveRotate, Space.Self);
+        }
+    }
+}
