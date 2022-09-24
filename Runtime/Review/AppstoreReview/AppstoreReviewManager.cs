9ddcbac33ca528fb710410c3817607eac786e9dd
Taewoo Lee <oiehot@gmail.com>
2022-09-24 17:59:50 +0900

    AppstoreReviewManager 개발.

---
+++ Runtime/Review/AppstoreReview/AppstoreReviewManager.cs
@@ -0,0 +1,36 @@
+﻿#nullable enable
+
+using System.Threading.Tasks;
+using UnityEngine.iOS;
+
+namespace Rano.Review.AppstoreReview
+{
+    /// <summary>
+    /// 애플 앱스토어 리뷰 전반을 관리한다.
+    /// </summary>
+    public sealed class AppstoreReviewManager : Rano.Review.ReviewManager
+    {
+        public override bool IsInitialized => true; // TODO: 개발 안됨
+        
+        public override bool Initialize()
+        {
+            Log.Info("초기화 중...");
+            Log.Info("초기화 완료");
+            return true;
+        }
+
+        public override async Task<bool> RequestReviewInternalAsync()
+        {
+            
+            Log.Info("리뷰 요청 중...");
+            bool result = Device.RequestStoreReview();
+            if (result == false)
+            {
+                Log.Warning("리뷰 요청 실패 (RequestStoreReview Failed)");
+                return false;
+            }
+            Log.Info("리뷰 요청 완료");
+            return true;
+        }
+    }
+}
